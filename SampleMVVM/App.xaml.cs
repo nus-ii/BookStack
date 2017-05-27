d51a4e1a7969575e79da864eb71ff86bb99bd1b3
nus-ii <nusii86@gmail.com>
2017-05-28 02:34:01 +0300

    12

---
+++ SampleMVVM/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using SampleMVVM.Models;
+using SampleMVVM.ViewModels;
+using SampleMVVM.Views;
+
+namespace SampleMVVM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            List<Book> books = new List<Book>()
+            {
+                new Book("Колобок", null, 3,@"C:\MyTempImg\2594300.jpg"),
+                new Book("CLR via C#", "Джеффри Рихтер", 1,@"C:\MyTempImg\1495061605.jpg"),
+                new Book("Война и мир", "Л.Н. Толстой", 2,@"C:\MyTempImg\Voyna_i_mir.jpg"),       
+
+            };
+
+            MainView view = new MainView(); // создали View
+            MainViewModel viewModel = new ViewModels.MainViewModel(books); // Создали ViewModel
+            view.DataContext = viewModel; // положили ViewModel во View в качестве DataContext
+            view.Show();
+        }
+    }
+}
---
+++ SampleMVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+using SampleMVVM.Commands;
+using System.Collections.ObjectModel;
+using SampleMVVM.Models;
+
+namespace SampleMVVM.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<BookViewModel> BooksList { get; set; }
+
+        public void SelectNext()
+        {     
+            SelectedBookIndex++;           
+            SelectedBook = BooksList.ElementAt(SelectedBookIndex);
+        }
+
+        private BookViewModel sBook;
+        public BookViewModel SelectedBook
+        {
+            get
+            {
+                return sBook;
+            }
+            set { sBook = value;
+                OnPropertyChanged("SelectedBook");
+            }
+        }
+
+        private int SelectedBookIndex;
+
+        #region Constructor
+
+        public MainViewModel(List<Book> books)
+        {
+            BooksList = new ObservableCollection<BookViewModel>(books.Select(b => new BookViewModel(b)));
+            SelectedBook = BooksList.ElementAt(0);
+            SelectedBookIndex = 0;
+        }
+
+        #endregion
+
+        private DelegateCommand selectNextCommand;
+
+        public ICommand SelectNextBookCommand
+        {
+            get
+            {
+                if (selectNextCommand == null)
+                {
+                    selectNextCommand = new DelegateCommand(SelectNext, CanSelectNext);
+                }
+                return selectNextCommand;
+            }
+        }
+
+        private bool CanSelectNext()
+        {
+            if (SelectedBookIndex < BooksList.Count-1)
+                return true;
+
+            return false;
+        }
+    }
+}
