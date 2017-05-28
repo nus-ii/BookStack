d30ff7d0dc33685eef226391da418322997083e0
nus-ii <nusii86@gmail.com>
2017-05-28 03:53:49 +0300

    424242

--- SampleMVVM/ViewModels/MainViewModel.cs
+++ SampleMVVM/ViewModels/MainViewModel.cs
@@ -15,23 +15,70 @@ namespace SampleMVVM.ViewModels
     {
         public ObservableCollection<BookViewModel> BooksList { get; set; }
 
-        public void SelectNext()
-        {     
-            SelectedBookIndex++;           
-            SelectedBook = BooksList.ElementAt(SelectedBookIndex);
-        }
-
-        private BookViewModel sBook;
-        public BookViewModel SelectedBook
+        public ObservableCollection<BookViewModel> SelectedBooksList
         {
             get
             {
-                return sBook;
+                return sbList;
             }
-            set { sBook = value;
-                OnPropertyChanged("SelectedBook");
+            set
+            {
+                this.sbList = value;
+                OnPropertyChanged("SelectedBooksList");
             }
         }
+
+        private ObservableCollection<BookViewModel> sbList;
+
+        private string fText;
+
+        public string filterText
+        {
+            get { return fText; }
+            set
+            {
+                fText = value;
+                SelectedBooksList = FilterFunction(fText);
+                OnPropertyChanged("filterText");
+            }
+        }
+
+        public ObservableCollection<BookViewModel> FilterFunction(string ftext)
+        {
+            ObservableCollection<BookViewModel> result = new ObservableCollection<BookViewModel>();
+
+            if (string.IsNullOrEmpty(fText))
+                return this.BooksList;
+
+            var t = fText.ToLower();
+            foreach (var b in this.BooksList)
+            {
+                if (b.Title.ToLower().Contains(t) || b.Author.ToLower().Contains(t))
+                {
+                    result.Add(b);
+                }
+            }
+
+            return result;
+        }
+
+        //public void SelectNext()
+        //{     
+        //    SelectedBookIndex++;           
+        //    SelectedBook = BooksList.ElementAt(SelectedBookIndex);
+        //}
+
+        //private BookViewModel sBook;
+        //public BookViewModel SelectedBook
+        //{
+        //    get
+        //    {
+        //        return sBook;
+        //    }
+        //    set { sBook = value;
+        //        OnPropertyChanged("SelectedBook");
+        //    }
+        //}
 
         private int SelectedBookIndex;
 
@@ -40,32 +87,33 @@ namespace SampleMVVM.ViewModels
         public MainViewModel(List<Book> books)
         {
             BooksList = new ObservableCollection<BookViewModel>(books.Select(b => new BookViewModel(b)));
-            SelectedBook = BooksList.ElementAt(0);
-            SelectedBookIndex = 0;
+            SelectedBooksList = BooksList;
+            // SelectedBook = BooksList.ElementAt(0);
+            //SelectedBookIndex = 0;
         }
 
         #endregion
 
-        private DelegateCommand selectNextCommand;
+        //private DelegateCommand selectNextCommand;
 
-        public ICommand SelectNextBookCommand
-        {
-            get
-            {
-                if (selectNextCommand == null)
-                {
-                    selectNextCommand = new DelegateCommand(SelectNext, CanSelectNext);
-                }
-                return selectNextCommand;
-            }
-        }
+        //public ICommand SelectNextBookCommand
+        //{
+        //    get
+        //    {
+        //        if (selectNextCommand == null)
+        //        {
+        //            selectNextCommand = new DelegateCommand(SelectNext, CanSelectNext);
+        //        }
+        //        return selectNextCommand;
+        //    }
+        //}
 
-        private bool CanSelectNext()
-        {
-            if (SelectedBookIndex < BooksList.Count-1)
-                return true;
+        //private bool CanSelectNext()
+        //{
+        //    if (SelectedBookIndex < BooksList.Count-1)
+        //        return true;
 
-            return false;
-        }
+        //    return false;
+        //}
     }
 }
